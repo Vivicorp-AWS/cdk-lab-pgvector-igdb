7e2ab07c87c823e130d98beeb9e8c918fd2ef65b
Violet Vivirand <purpleslovemail@gmail.com>
2023-07-14 20:38:28 +0800

    Remove unnecessary prefix parameter

--- stacks/s3_stack.py
+++ stacks/s3_stack.py
@@ -11,22 +11,24 @@ from constructs import Construct
 
 class S3Stack(Stack):
 
-    def __init__(self, scope: Construct, id: str, prefix:str, **kwargs) -> None:
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        self.bucket = s3.Bucket(self, "AssetsBucket",
-                           removal_policy=RemovalPolicy.DESTROY,
-                           auto_delete_objects=True,)
+        self.bucket = s3.Bucket(
+            self, "AssetsBucket",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,)
 
         # Upload assets to bucket
-        s3deploy.BucketDeployment(self, f"AssetsDeployment",
-                                  sources=[s3deploy.Source.asset("./assets")],
-                                  destination_bucket=self.bucket,
-                                  memory_limit=256,
-                                  ephemeral_storage_size=Size.mebibytes(1024),
-                                  retain_on_delete=True,
-                                  prune=False,
-                                  )
+        s3deploy.BucketDeployment(
+            self, f"AssetsDeployment",
+            sources=[s3deploy.Source.asset("./assets")],
+            destination_bucket=self.bucket,
+            memory_limit=256,
+            ephemeral_storage_size=Size.mebibytes(1024),
+            retain_on_delete=True,
+            prune=False,
+            )
 
         CfnOutput(self, "BucketName", value=self.bucket.bucket_name)
         CfnOutput(self, "BucketARN", value=self.bucket.bucket_arn)
